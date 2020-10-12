88faa36b279681242d361f74bd1ca1a28e2a2eed
venkey <venkeyvenkatesh885@gmail.com>
2020-10-12 11:01:55 +0530

    [venkey]Added custom exception to handle null and empty string inputs

---
+++ MoodAnalyserApplication/MoodAnalysisException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserApplication
+{
+  public class MoodAnalysisException:ApplicationException
+    {
+        public enum ExceptionType
+        {
+            ENTERED_NULL, ENTERED_EMPTY
+        }
+
+        public ExceptionType type;
+
+        public MoodAnalysisException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
